2d4ca76fbd1e343479a05de7e16771fe954e0a33
nklenke <nick_klenke@yahoo.com>
2020-05-18 17:21:31 -0500

    change to use postgres

--- models.py
+++ models.py
@@ -1,33 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 import os
+
+# If running locally or running unit tests, use the sqllite database
 '''
-database_name = "casting"
-# database_path = "postgres://{}/{}".format('localhost:5432', database_name)
-database_path = "postgres://{}:{}@{}/{}".format(
-    'postgres', 'willa2018', 'localhost:5432', database_name)
-
-db = SQLAlchemy()
-
-def setup_db(app, database_path=database_path):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-    db.create_all()
-'''
-
 database_filename = "database.db"
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+'''
+# If running in production use postgres on heroku
+database_path = os.environ.get('DATABASE_URL', 'postgres://postgres:willa2018@localhost:5432/casting_agency')
 
 db = SQLAlchemy()
-'''
-def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    db.app = app
-    db.init_app(app)
-'''
+
 def setup_db(app, database_path=database_path):
     app.config["SQLALCHEMY_DATABASE_URI"] = database_path
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
