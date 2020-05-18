5c6d7d19c3878f4b157344f4ad26500180c55810
nklenke <nick_klenke@yahoo.com>
2020-05-18 17:47:19 -0500

    setting up migrations

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def create_app(test_config=None):
   app = Flask(__name__)
   setup_db(app)
   CORS(app)
-  db_drop_and_create_all()
+  # db_drop_and_create_all()
 
   @app.after_request
   def after_request(response):
